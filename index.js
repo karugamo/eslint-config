8d1d9ad755b23263783c800a91355b1c8d6f96d9
Finn Pauls <mbp@pauls.dev>
2020-08-08 13:46:05 +0200

    Add eslint recommended

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ module.exports = {
       es2020: true
     },
     extends: [
+      'eslint:recommended',
       'plugin:react/recommended',
       'plugin:react-hooks/recommended',
       'prettier',
